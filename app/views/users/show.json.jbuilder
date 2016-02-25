8b67ae31d5e901f5be7192e412d6bc1be5dc5ff0
Your Name <you@example.com>
2016-02-25 16:59:02 +0900

    Add MasterTable

--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @user, :id, :id, :name, :birthday, :phone1, :phone2, :post_number, :address, :other, :created_at, :updated_at
+json.extract! @user, :id, :name, :birthday, :phone1, :phone2, :post_number, :address, :other, :created_at, :updated_at
--- db/migrate/20160225072044_create_users.rb
+++ db/migrate/20160225072044_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.Date :birthday
-      t.string :phone1
+      t.string :name, :null => false
+      t.date :birthday, :null => false
+      t.string :phone1, :null => false
       t.string :phone2
-      t.string :post_number
-      t.string :address
+      t.string :post_number, :null => false
+      t.string :address, :null => false
       t.text :other
 
       t.timestamps null: false
